1a047f4cd19230ff9df2bc9bcff960c42879492e
IRIDIUM-SUB <novapotassium@gmail.com>
2022-06-16 22:47:44 +0800

    docs: Update Usage and Output feat: 解析函数的变量并存储到设计的数据结构中准备分析

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # parasTracer
 一个用于[Massive Code Runner](https://github.com/iridium-soda/massive-coderunner)的原理验证小项目.用于追踪函数参数的传播.
+# Description
+检查函数中每个参数的传播路径,返回和每个函数形参*相关*的变量.
+> 相关:被形参和形参相关的变量赋值或为相关运算的返回结果,或作为形参传入函数的返回值.
+
+Example:
+
+```go
+package main
+
+func funcA(parA int, parB int, parC int)int {
+	var a = parA + parB
+	var b=a+parC
+	var d=funcB(a,b)
+	return d
+	
+}
+func funcB(a int,b int)int{
+	return a+b
+}
+func main() {
+	funcA(1,2,3)
+}
+```
+则分析结果应为:
+- 和`parA`相关的变量有:`a`,`b`(由`a`经由`+`传导),d(由`a`或`b`经由`funcB`传导)
+- 和`parB`相关的变量有:`a`,`b`,`d`
+- 和`parC`相关的变量有:`b`,`d`
+# Usage
+```shell
+parasTracer <filename>
+```
+Output:
+```
+funcName: <Name> paraName:<Name> releated vars:[<Name>,...]
+```
---
+++ cmd/parasTracer/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"os"
+)
+
+var fileName string
+var usage string = `To analysis and trace paras of functions.
+Usage:
+parasTracer <filename>`
+
+func printUsage() {
+	fmt.Fprintln(os.Stderr, usage)
+	return
+}
+
+func main() {
+	flag.Parse()
+	if flag.NArg() != 1 { //Error handle
+		printUsage()
+		return
+	}
+	fileName = flag.Args()[0]
+	doAnalysis()
+}
+func doAnalysis() {
+	//解析最上一层的顶级声明,再将函数信息传入下一层函数分析
+	fileSet := token.NewFileSet()
+	f, err := parser.ParseFile(fileSet, fileName, "", 0)
+	if err != nil {
+		fmt.Printf("err = %s", err)
+	}
+	for _, i := range f.Decls {
+		fn, ok := i.(*ast.FuncDecl)
+		if ok { //Got Function
+			fmt.Printf("Got function decl, name is %s", fn.Name.Name)
+			analysisFunc(fn)
+		}
+
+	}
+}
+func analysisFunc(fn *ast.FuncDecl) {
+	//分析单个函数的传播链
+	//获取形参
+	//注意形如 s0, s1 string, s2 string这样的声明, s0和s1是在一个field里所以需要再加一个循环解析
+	var parasList []string //暂存参数表
+	for _, paraField := range fn.Type.Params.List {
+		for _, singlePara := range paraField.Names {
+			fmt.Printf("Got function para, name is %s", singlePara.Name)
+			parasList = append(parasList, singlePara.Name)
+		}
+	}
+	//建立参数传播存储结构
+	//每个list第一个为形参(变量传染根),后面的应为不重复的变量名.
+	var parasInfo [][]string
+	for index, paraName := range parasList {
+		parasInfo = append(parasInfo, make([]string, 1))
+		parasInfo[index][0] = paraName
+	}
+	//此时parasInfo应该为[[a],[b],[c],...]
+
+	//准备分析,应该是FuncDecl.Body
+	for _, _ = range fn.Body.List {
+		//TODO:考虑内部仍然有代码块的情况
+		//应该只有stmt类型和BlockStmt类型
+
+	}
+}
